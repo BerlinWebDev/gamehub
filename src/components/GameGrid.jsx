166988f539b6c4c893d997341c52a9af71e98a1b
BerlinWebDev <berlinwebentwickler@gmail.com>
2023-05-27 23:57:21 +0200

    Created Custom Hooks useGames

--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -1,19 +1,7 @@
 import { Text } from "@chakra-ui/react";
-import { useState, useEffect } from "react";
-import apiClient from "../services/api-client";
-
+import useGames from "../hooks/useGames";
 function GameGrid() {
-  const [games, setGames] = useState([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get("/games", {
-        signal: controller.signal,
-      })
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  }, []);
+  const { games, error } = useGames();
 
   return (
     <>
