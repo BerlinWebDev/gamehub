d8486182a30843a3ec8a0355a9573c19b46ef498
BerlinWebDev <berlinwebentwickler@gmail.com>
2023-06-03 13:15:39 +0200

    Added Genres

---
+++ src/hooks/useGenre.js
@@ -0,0 +1,33 @@
+import { CanceledError } from "axios";
+import { useState, useEffect } from "react";
+import apiClient from "../services/api-client";
+
+const useGenre = () => {
+  const [genres, setGenres] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
+
+    apiClient
+      .get("/genres", {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setGenres(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+  return { genres, error, loading };
+};
+
+export default useGenre;
