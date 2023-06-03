2e0efbd8f350505fa54d175bb05e823c1d5346cf
BerlinWebDev <berlinwebentwickler@gmail.com>
2023-06-03 13:32:57 +0200

    fixed max-width

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,14 @@ import GenreList from "./components/GenreList";
 function App() {
   return (
     <Grid
-      templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "250px 1fr",
+      }}
     >
       <GridItem area="nav">
         <NavBar />
