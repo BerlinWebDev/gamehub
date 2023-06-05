3974a4fdbf6e5c26b6e3184cc638a01bedb60719
BerlinWebDev <berlinwebentwickler@gmail.com>
2023-06-05 23:19:48 +0200

    Showing Spinner while loading genres

--- src/components/GenreList.jsx
+++ src/components/GenreList.jsx
@@ -1,10 +1,15 @@
-import { HStack, List, ListItem, Text, Image } from "@chakra-ui/react";
+import { HStack, List, ListItem, Text, Image, Spinner } from "@chakra-ui/react";
 import React from "react";
 import useGenre from "../hooks/useGenre";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data } = useGenre("/genres");
+  const { data, loading, error } = useGenre("/genres");
+
+  if (error) return null;
+
+  if (loading) return <Spinner />;
+
   return (
     <List>
       {data.map((genre) => (
