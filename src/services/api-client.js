409afed34596c5a85a1ce3e4468cb43c230183ac
BerlinWebDev <berlinwebentwickler@gmail.com>
2023-06-19 23:09:30 +0200

    Added ENV Variable

--- src/services/api-client.js
+++ src/services/api-client.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "582451642d8c4814b3912763f6c8a890",
+    key: import.meta.env.VITE_REACT_APP_API_KEY,
   },
 });
